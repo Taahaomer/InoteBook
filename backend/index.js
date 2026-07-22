5e14ebf88d1e3a2fa9e0ca831860e19e8505a8eb
Taahaomer <taahaomer555@gmail.com>
2026-07-22 20:16:53 +0500

    Fix CORS for frontend deployment

--- backend/index.js
+++ backend/index.js
@@ -13,11 +13,11 @@ const PORT = process.env.PORT || 5000;
 // app.use(cors())
 app.use(cors({
     origin: [
-        "https://inote-book-3gobcsbio-taaha-omer.vercel.app",
+        "https://inote-book-k6l3tbrsh-taaha-omer.vercel.app",
         "http://localhost:3000"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
+    allowedHeaders: ["Content-Type", "auth-token"]
 }));
 app.use(express.json())
 
