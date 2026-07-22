893847ae5acf04560024ceb786cd978eb4ba5567
Taahaomer <taahaomer555@gmail.com>
2026-07-22 16:02:33 +0500

    Prepare backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,7 @@
+const dotenvResult = require('dotenv').config({ path: require('path').resolve(__dirname, '.env') });
+console.log('DOTENV DEBUG:', dotenvResult);
+console.log('MONGO_URI value:', process.env.MONGO_URI);
+
 const connectToMongo = require("./db");
 const express = require("express");
 var cors = require('cors')
