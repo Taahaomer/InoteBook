ff4c06b79f28c6b847a6ebe7eb8a03485ddcbc5d
Taahaomer <taahaomer555@gmail.com>
2026-07-28 22:27:21 +0500

    fd

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -14,6 +14,7 @@ const NoteItem = (props) => {
                 <h5 className="card-title">{note.title}</h5>
                 <p className="card-text">{note.description}</p>
                 <div className="d-flex justify-content-between ">
+                    {console.log(note.tags)}
                     {(note.tags[0]!==null) &&  <div className='d-flex gap-1'>
                         {note.tags.map((tag)=>{ return <p role="button" className={"border border-dark p-1 rounded"}>{tag}</p>  })}
                     </div>}
